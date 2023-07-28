63564a72d970afb4b76a5270eab258f1d1516768
María José Valverde Peña <mariajosevalverdepena@gmail.com>
2023-07-28 12:06:42 -0600

    Se realizó pull del código de los compañeros, se eliminó el txt que se veía en "ListaEnlazadaSimple" para pasarlo a una clase java debido a esto se comento toda esta clase hasta el momento debido a que tiene un error en la clase nodo , falta que Daniel agregue la clase y también lo implementé al menú RECUERDE DESCOMENTAR LA CLASE

--- src/estructurasproyectoiic23/InterfazRegistroProductos.java
+++ src/estructurasproyectoiic23/InterfazRegistroProductos.java
@@ -102,4 +102,6 @@ public class InterfazRegistroProductos {
         cola.ordenarZA();
         cola.imprimirCola();
     }
-}
+}  
+
+
--- src/estructurasproyectoiic23/ListaEnlazadaSimple.java
+++ src/estructurasproyectoiic23/ListaEnlazadaSimple.java
@@ -1,10 +1,9 @@
-package estructurasproyectoiic23;
+/* package estructurasproyectoiic23;
 
 public class ListaEnlazadaSimple {
     public ListaEnlazadaSimple() {
         inicio = null;
     }
-
     public void modificarProducto(int indice, Producto nuevoProducto) {
         if (indice < 0 || inicio == null) {
             throw new IndexOutOfBoundsException("Índice fuera de rango o lista vacía");
@@ -18,4 +17,8 @@ public class ListaEnlazadaSimple {
         }
         temp.producto = nuevoProducto;
     }
-}
+} */
+
+//Se comento toda la clase hasta el momento debido a que tiene un error en la clase nodo , falta que Daniel agregue 
+//la clase y también lo implementé al menú
+//RECUERDE DESCOMENTAR LA CLASE
